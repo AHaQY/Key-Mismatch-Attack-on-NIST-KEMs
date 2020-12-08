5b59ca5ccfa9edbc3715eb0cba7441e21cb5287e
zxh <33541883+SherZXH@users.noreply.github.com>
2020-12-08 20:59:11 +0800

    Update test_kem.c

--- Saber_key_mismatch_attack/Firesaber/test/test_kem.c
+++ Saber_key_mismatch_attack/Firesaber/test/test_kem.c
@@ -12,7 +12,7 @@
 
 static int test_pke_cpa()
 {
-	//key mismatch attack on Saber
+	//key mismatch attack on Firesaber
    // parameters: res: b'
    //          vprime: cm
    //            ss_a: message
